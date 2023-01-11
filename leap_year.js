d43aa615b70e152fa6b1efe1d44ecc2bb551d094
Proshanta Roy <71019355+Proshanta15@users.noreply.github.com>
2023-01-11 13:04:18 +0600

    Update leap_year.js

--- leap_year.js
+++ leap_year.js
@@ -3,9 +3,9 @@ function checkLeapYear(year) {
 
     //three conditions to find out the leap year
     if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
-        console.log(year + ' is a leap year');
+        console.log(true);
     } else {
-        console.log(year + ' is not a leap year');
+        console.log(false);
     }
 }
 
